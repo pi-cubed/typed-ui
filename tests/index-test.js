ae2410dc080d24d01c013f83a25fc946803fa260
Dylan Richardson <dylanrichardson1996@gmail.com>
2018-05-15 11:18:24 -0400

    test(Primitive IO): Add fuzzer library. Add fuzz test for primitiveOutput

--- tests/index-test.js
+++ tests/index-test.js
@@ -2,22 +2,24 @@ import expect from 'expect';
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import PrimitiveOutput from '../src/PrimitiveOutput';
+import { mutate } from 'fuzzer';
 
 describe('PrimitiveOutput', () => {
   let node;
-  const x = 'hew';
+  let data;
 
   beforeEach(() => {
     node = document.createElement('div');
+    data = mutate.string('abc123');
   });
 
   afterEach(() => {
     unmountComponentAtNode(node);
   });
 
-  it('displays data', () => {
-    render(<PrimitiveOutput data={x} />, node, () => {
-      expect(node.innerHTML).toContain(x);
-    });
-  });
+  it('displays data', () => 
+    render(<PrimitiveOutput data={data} />, node, () =>
+      expect(node.innerHTML).toContain(data)
+    )
+  );
 });
